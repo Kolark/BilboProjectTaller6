87604dfc138fd9d793a88c36597da0d72fb40bfc
Kolark <47009873+Kolark@users.noreply.github.com>
2020-08-07 22:31:50 -0500

    progreso

---
+++ Assets/Scripts/UiScripts/ButtonLoadScene.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class ButtonLoadScene : MonoBehaviour
+{
+    //Carga una escena desde un boton.
+    public void LoadScene(int scene)
+    {
+        SceneManager.LoadScene(scene);
+    }
+}
