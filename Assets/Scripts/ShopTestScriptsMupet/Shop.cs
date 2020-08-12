615a9d4a0ffd8e421b2ce049a0ae174462855291
Mupet <mupetcol@gmail.com>
2020-08-12 09:19:41 -0500

    ShopScene y ShopBaseUI

---
+++ Assets/Scripts/ShopTestScriptsMupet/Shop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    GameObject itemTemplate;
+    GameObject g;
+    [SerializeField]Transform shopScrollView;
+
+    private void Start()
+    {
+        itemTemplate = shopScrollView.GetChild(0).gameObject;
+
+        for (int i = 0; i < 10; i++)
+        {
+            g = Instantiate(itemTemplate, shopScrollView);
+        }
+
+        Destroy(itemTemplate);
+    }
+}
