f018591599be986ceb07de4c849837d9848bd730
Mupet <mupetcol@gmail.com>
2020-08-14 00:25:14 -0500

    ShopProgress Cambios UI según escena cargada

---
+++ Assets/Scripts/ShopTestScriptsMupet/HUDChanger.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class HUDChanger : MonoBehaviour
+{
+    #region Singleton.HUDChanger
+    public static HUDChanger instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+    #endregion
+
+    public Sprite[] imagenesHUD;
+    //[SerializeField] Canvas canv;
+    Image leftArrow;
+    Image rightArrow;
+    Image past;
+    Image present;
+    Image future;
+    Image jump;
+
+    Canvas canv;
+
+    private void Start()
+    {
+        imagenesHUD = new Sprite[6];
+    }
+    // Update is called once per frame
+    void OnEnable()
+    {
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    // called second
+    void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        if(scene.buildIndex == 3)
+        {
+            canv = FindObjectOfType<Canvas>();
+            SetEquipedHUD();
+        }
+        
+    }
+
+    public void SetEquipedHUD()
+    {
+        leftArrow = canv.transform.GetChild(1).GetChild(1).GetChild(1).GetComponent<Image>();
+        rightArrow = canv.transform.GetChild(1).GetChild(1).GetChild(0).GetComponent<Image>();
+        past = canv.transform.GetChild(0).GetChild(0).GetComponent<Image>();
+        present = canv.transform.GetChild(0).GetChild(1).GetComponent<Image>();
+        future = canv.transform.GetChild(0).GetChild(2).GetComponent<Image>();
+        jump = canv.transform.GetChild(1).GetChild(0).GetComponent<Image>();
+
+        leftArrow.sprite = imagenesHUD[0];
+        rightArrow.sprite = imagenesHUD[1];
+        past.sprite = imagenesHUD[2];
+        present.sprite = imagenesHUD[3];
+        future.sprite = imagenesHUD[4];
+        jump.sprite = imagenesHUD[5];
+    }
+
+}
---
+++ Assets/Scripts/ShopTestScriptsMupet/Testing.cs
@@ -0,0 +1,41 @@
+﻿
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Testing : MonoBehaviour
+{
+    [SerializeField] Sprite[] imagenesHUD;
+    [SerializeField] Canvas canv;
+    Image leftArrow;
+    Image rightArrow;
+    Image past;
+    Image present;
+    Image future;
+    Image jump;
+
+    void Start()
+    {
+        ChangeCanvasHUDSprites();
+        
+    }
+
+    // Update is called once per frame
+    void ChangeCanvasHUDSprites()
+    {
+        leftArrow = canv.transform.GetChild(1).GetChild(1).GetChild(1).GetComponent<Image>();
+        rightArrow = canv.transform.GetChild(1).GetChild(1).GetChild(0).GetComponent<Image>();
+        past = canv.transform.GetChild(0).GetChild(0).GetComponent<Image>();
+        present = canv.transform.GetChild(0).GetChild(1).GetComponent<Image>();
+        future = canv.transform.GetChild(0).GetChild(2).GetComponent<Image>();
+        jump = canv.transform.GetChild(1).GetChild(0).GetComponent<Image>();
+
+        leftArrow.sprite = imagenesHUD[0];
+        rightArrow.sprite = imagenesHUD[1];
+        past.sprite = imagenesHUD[2];
+        present.sprite = imagenesHUD[3];
+        future.sprite = imagenesHUD[4];
+        jump.sprite = imagenesHUD[5];
+    }
+}
