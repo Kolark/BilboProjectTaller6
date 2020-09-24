7c55cecf09cb9cfc3c1e514f415e3e0bb0bbf6b9
Kolark <47009873+Kolark@users.noreply.github.com>
2020-09-24 01:30:49 -0500

    progreso

--- Assets/Scripts/Estalactita.cs
+++ Assets/Scripts/Estalactita.cs
@@ -1,11 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+[RequireComponent(typeof(RayActivatedEvent))]
 public class Estalactita : MonoBehaviour
 {
+    RayActivatedEvent rayActivatedEvent;
+    Rigidbody2D rb2d;
+    Collider2D col2d;
     private void Awake()
     {
-        
+        rb2d = GetComponent<Rigidbody2D>();
+        col2d = GetComponent<Collider2D>();
+        rayActivatedEvent = GetComponent<RayActivatedEvent>();
+        rayActivatedEvent.EventToActivate += Drop;
+    }
+    void Drop()
+    {
+        col2d.enabled = true;
+        rb2d.bodyType = RigidbodyType2D.Dynamic;
+        rb2d.AddForce(-Vector2.up*10, ForceMode2D.Impulse);
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.transform.CompareTag("Ground"))
+        {
+            rb2d.bodyType = RigidbodyType2D.Static;
+        };
     }
 }
