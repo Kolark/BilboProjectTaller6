2632ee869b15c957f7cf04b4654815464c587393
Kolark <47009873+Kolark@users.noreply.github.com>
2020-09-23 21:17:59 -0500

    progreso

---
+++ Assets/Scripts/Estalactita.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Estalactita : MonoBehaviour
+{
+    private void Awake()
+    {
+        
+    }
+}
