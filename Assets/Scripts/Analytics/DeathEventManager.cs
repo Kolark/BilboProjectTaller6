e47b31cfb404c54e2b4f0716e61a2ed34c0ec379
Mupet <mupetcol@gmail.com>
2020-10-04 17:12:39 -0500

    Analytics lvl1 y lvl2

---
+++ Assets/Scripts/Analytics/DeathEventManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class DeathEventManager : MonoBehaviour
+{
+    public int DeathID;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Analytics.CustomEvent("PuzzleTimed", new Dictionary<string, object>
+        {
+            {"Puzzle",DeathID},
+        });
+    }
+
+
+
+}
