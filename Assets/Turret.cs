f02cb559ad5639cc26e3d6aec234bb2775e05503
ZeritFreeman <juanc.sierram@upb.edu.co>
2020-10-05 17:32:33 -0500

    Torreta Gamer
    Solo apunta, aun no dispara

---
+++ Assets/Turret.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    public float speed = 5f;
+
+    void Update()
+    {
+        Aim(Movement2D.Instance.transform.position);
+    }
+
+    void Aim(Vector3 playerPosition)
+    {
+        Vector2 direction = playerPosition - transform.position;
+        float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+        Quaternion rotation = Quaternion.AngleAxis(angle, Vector3.forward);
+        transform.rotation = Quaternion.Slerp(transform.rotation, rotation, speed * Time.deltaTime);
+    }
+}
