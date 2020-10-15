75c83adc5963ba32c21a40151ab273b8ed98043b
ZeritFreeman <juanc.sierram@upb.edu.co>
2020-10-15 17:29:28 -0500

    Spear
    Se agrego una referencia para la distancia que sube y baja

--- Assets/Spear.cs
+++ Assets/Spear.cs
@@ -7,13 +7,15 @@ public class Spear : MonoBehaviour
 {
     [SerializeField] float speedUp = 5f;
     [SerializeField] float speedDown = 5f;
+    [SerializeField] float distanceUp = 1f;
+    [SerializeField] float distanceDown = 1f;
     Vector2 toPosition1;
     Vector2 toPosition2;
     private void Start()
     {
-        toPosition1 = Vector2.up;
+        toPosition1 = Vector2.up * distanceUp;
         toPosition1.x = transform.position.x;
-        toPosition2 = Vector2.down;
+        toPosition2 = Vector2.down * distanceDown;
         toPosition2.x = transform.position.x;
         Raise();
     }
