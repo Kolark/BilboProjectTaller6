000cfa2ee91674df5e043dd54e6d738052ddd824
ZeritFreeman <juanc.sierram@upb.edu.co>
2020-10-20 11:42:27 -0500

    Hasta el 4 melo
    Falta audios del 5

--- Assets/Spear.cs
+++ Assets/Spear.cs
@@ -21,7 +21,7 @@ public class Spear : MonoBehaviour
 
     void Raise()
     {
-        AudioManager.instance.Play("Lanza");
+        //AudioManager.instance.Play("Lanza");
         transform.DOMove(initPos+Vector2.up*distanceUp, 1/speedUp, false).OnComplete(() => { Down(); });
     }
 
