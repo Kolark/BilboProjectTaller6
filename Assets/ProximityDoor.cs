a54a29ddd090ba81e2c6af4d5a1c5c8473d76603
ZeritFreeman <juanc.sierram@upb.edu.co>
2020-10-22 00:09:45 -0500

    Puerta
    Se abre y cierra con proximidad

---
+++ Assets/ProximityDoor.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProximityDoor : MonoBehaviour
+{
+    Animator animator;
+    [SerializeField]float radius = 10f;
+    Collider2D collider;
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        collider = GetComponent<Collider2D>();
+    }
+
+    private void Update()
+    {
+        if (Vector2.Distance(transform.position, Movement2D.Instance.transform.position) < radius)
+        {
+            Open();
+        }
+        else
+        {
+            Close();
+        }
+    }
+
+    void Open()
+    {
+        animator.SetBool("inRange", true);
+        collider.enabled = false;
+    }
+
+    void Close()
+    {
+        animator.SetBool("inRange", false);
+        collider.enabled = true;
+    }
+}
