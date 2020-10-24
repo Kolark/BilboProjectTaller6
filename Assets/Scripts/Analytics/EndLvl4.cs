be03347e557124ba35e5328cdec3caa337448657
Mupet <mupetcol@gmail.com>
2020-10-23 21:06:25 -0500

    CambioIdiomaAInglés
    Falta cambiar los gifs, el del switch especialmente.

---
+++ Assets/Scripts/Analytics/EndLvl4.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+public class EndLvl4 : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Analytics.CustomEvent("Lvl4Winner");
+    }
+}
---
+++ Assets/Scripts/Analytics/EndLvl5.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Analytics;
+public class EndLvl5 : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Analytics.CustomEvent("Lvl5Winner");
+    }
+}
