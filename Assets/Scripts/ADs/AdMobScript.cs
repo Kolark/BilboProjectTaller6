28fe18e5af601096498746129280067d47d1cf82
Mupet <mupetcol@gmail.com>
2020-10-29 14:23:56 -0500

    cambiesitos

---
+++ Assets/Scripts/ADs/AdMobScript.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+using GoogleMobileAds.Api;
+
+public class AdMobScript : MonoBehaviour
+{
+    public Text status;
+    string App_ID = "ca-app-pub-8309191909587214~7610171797";
+    string Interstitial_Ad_ID = "ca-app-pub-3940256099942544/1033173712";
+
+    private InterstitialAd interstitial;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MobileAds.Initialize(App_ID);
+    }
+
+    private void RequestInterstitial()
+    {
+        // Initialize an InterstitialAd.
+        this.interstitial = new InterstitialAd(Interstitial_Ad_ID);
+
+        // Called when an ad request has successfully loaded.
+        this.interstitial.OnAdLoaded += HandleOnAdLoaded;
+        // Called when an ad request failed to load.
+        this.interstitial.OnAdFailedToLoad += HandleOnAdFailedToLoad;
+        // Called when an ad is shown.
+        this.interstitial.OnAdOpening += HandleOnAdOpened;
+        // Called when the ad is closed.
+        this.interstitial.OnAdClosed += HandleOnAdClosed;
+        // Called when the ad click caused the user to leave the application.
+        this.interstitial.OnAdLeavingApplication += HandleOnAdLeavingApplication;
+
+        AdRequest request = new AdRequest.Builder().Build();
+        this.interstitial.LoadAd(request);
+    }
+
+    public void ShowInterstetialAd()
+    {
+        if (this.interstitial.IsLoaded())
+        {
+            this.interstitial.Show();
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        RequestInterstitial();
+    }
+    //FOR EVENTS AND DELEGATES FOR ADS 
+
+    public void HandleOnAdLoaded(object sender, EventArgs args)
+    {
+        //status.text = "Ad loaded";
+        ShowInterstetialAd();
+    }
+
+    public void HandleOnAdFailedToLoad(object sender, AdFailedToLoadEventArgs args)
+    {
+        //status.text = "Ad failed to load";
+    }
+
+    public void HandleOnAdOpened(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdOpened event received");
+    }
+
+    public void HandleOnAdClosed(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdClosed event received");
+    }
+
+    public void HandleOnAdLeavingApplication(object sender, EventArgs args)
+    {
+        MonoBehaviour.print("HandleAdLeavingApplication event received");
+    }
+}
